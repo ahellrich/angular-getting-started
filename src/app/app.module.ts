52ed973644c0a6ff527d730899b856194d5b1af2
ahellrich <andreas.hellrich@googlemail.com>
2022-03-23 12:03:36 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
+
 @NgModule({
   imports: [
     BrowserModule,
